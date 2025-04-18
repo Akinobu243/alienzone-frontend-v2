95cbd48e4e6371cf3bf13ae1060143afd71040f8
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2025-04-18 18:54:00 +0500

    chore: remove unused import

--- app/(pages)/friends/page.tsx
+++ app/(pages)/friends/page.tsx
@@ -2,7 +2,6 @@
 
 import { Suspense, useState } from "react"
 import Link from "next/link"
-import { useAliens } from "@/store/hooks"
 
 import { cn } from "@/lib/utils"
 import IconButton from "@/components/ui/icon-button"
