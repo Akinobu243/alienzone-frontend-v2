1c1c0424c1307de7a7fc5127f992cad8f0c2fe17
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2025-06-04 19:41:50 +0500

    feat: add journal page

---
+++ components/pages/journal/data.tsx
@@ -0,0 +1,82 @@
+export const journalData = [
+  {
+    id: 1,
+    type: "dojo",
+    image:
+      "https://alienzone-v2.s3.dualstack.us-west-1.amazonaws.com/traits/eyes/choroi.png",
+  },
+  {
+    id: 2,
+    type: "dojo",
+    image:
+      "https://alienzone-v2.s3.dualstack.us-west-1.amazonaws.com/traits/hair/prisoner.png",
+  },
+  {
+    id: 2,
+    type: "dojo",
+    image:
+      "https://alienzone-v2.s3.dualstack.us-west-1.amazonaws.com/traits/eyes/majime-blue.png",
+  },
+  {
+    id: 2,
+    type: "dojo",
+    image:
+      "https://alienzone-v2.s3.dualstack.us-west-1.amazonaws.com/traits/hair/kusege.png",
+  },
+  {
+    id: 2,
+    type: "dojo",
+    image:
+      "https://alienzone-v2.s3.dualstack.us-west-1.amazonaws.com/traits/eyes/konryoku-orange-green.png",
+  },
+
+  {
+    id: 6,
+    type: "gear",
+    image:
+      "https://alienzone-v2.s3.amazonaws.com/gear-items/Daffodil%20(Tembin).png",
+  },
+  {
+    id: 7,
+    type: "gear",
+    image:
+      "https://alienzone-v2.s3.amazonaws.com/gear-items/Woolball%20(Shishi%20Cat).png",
+  },
+  {
+    id: 8,
+    type: "gear",
+    image:
+      "https://alienzone-v2.s3.amazonaws.com/gear-items/Eternal%20Ice%20(The Twins).png",
+  },
+  {
+    id: 9,
+    type: "gear",
+    image:
+      "https://alienzone-v2.s3.amazonaws.com/gear-items/Seal%20of%20the%20mystic%20guard%20(Karushi).png",
+  },
+  {
+    id: 10,
+    type: "gear",
+    image:
+      "https://alienzone-v2.s3.amazonaws.com/gear-items/Flask%20(Dante).png",
+  },
+  {
+    id: 11,
+    type: "gear",
+    image:
+      "https://alienzone-v2.s3.amazonaws.com/gear-items/Nano-Quantic%20Reactor%20(Nikola).png",
+  },
+
+  {
+    id: 12,
+    type: "raid",
+    image:
+      "https://alienzone-v2.s3.us-west-1.amazonaws.com/characters/Portal+1/Team/Stage+2/ALPHA+2.png",
+  },
+  {
+    id: 13,
+    type: "raid",
+    image:
+      "https://alienzone-v2.s3.us-west-1.amazonaws.com/characters/Portal+1/Team/Stage+3/ALPHA+3.png",
+  },
+]
---
+++ components/pages/journal/index.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from "react"
+import Image from "next/image"
+import { CloudLightning } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+import { journalData } from "./data"
+
+const JournalTabs = [
+  {
+    label: "All",
+    value: "all",
+    icon: CloudLightning,
+  },
+  {
+    label: "Alien Gear",
+    value: "gear",
+    icon: CloudLightning,
+  },
+  {
+    label: "Alien Raid",
+    value: "raid",
+    icon: CloudLightning,
+  },
+  {
+    label: "Dojo Items",
+    value: "dojo",
+    icon: CloudLightning,
+  },
+]
+
+const JournalPage = () => {
+  const [activeTab, setActiveTab] = useState<string>("all")
+  const [filteredItems, setFilteredItems] = useState<any[]>(journalData)
+
+  // Handle tab change
+  const handleTabChange = (tabValue: string) => {
+    setActiveTab(tabValue)
+  }
+
+  // Filter inventory items based on the selected tab
+  useEffect(() => {
+    if (activeTab === "all") {
+      setFilteredItems(journalData)
+    } else if (activeTab === "gear") {
+      setFilteredItems(journalData?.filter((item) => item.type === "gear"))
+    } else if (activeTab === "raid") {
+      setFilteredItems(journalData?.filter((item) => item.type === "raid"))
+    } else if (activeTab === "dojo") {
+      setFilteredItems(journalData?.filter((item) => item.type === "dojo"))
+    }
+  }, [activeTab])
+
+  return (
+    <div className="relative w-full h-full">
+      <div className="relative w-full h-full bg-white/5 border border-white/10 rounded-xl p-3 flex flex-col lg:flex-row gap-3 overflow-hidden">
+        <div
+          className={cn(
+            "w-full h-full bg-white/5 backdrop-blur-lg rounded p-4 overflow-hidden block"
+          )}
+        >
+          {/* Tabs - scrollable on small screens */}
+          <div className="flex gap-2 max-lg:flex-wrap pb-2 scrollbar-hide">
+            {JournalTabs.map((tab) => (
+              <button
+                key={tab.value}
+                onClick={() => handleTabChange(tab.value)}
+                className={cn(
+                  "px-3 sm:px-4 h-10 lg:w-full rounded-lg border border-white/10 whitespace-nowrap flex items-center gap-2 justify-between text-xs sm:text-sm transition-all duration-300",
+                  activeTab === tab.value ? "bg-white/20" : "bg-white/5"
+                )}
+              >
+                <span>{tab.label}</span>
+                <tab.icon className="w-4 h-4 hidden sm:block" />
+              </button>
+            ))}
+          </div>
+
+          <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-3 mt-4 pb-20 h-full overflow-y-auto">
+            {filteredItems.map((item, index) => (
+              <div
+                key={index}
+                className={cn(
+                  "w-full h-full backdrop-blur-lg bg-white/5 rounded-lg p-3 cursor-pointer transition-all duration-200 hover:bg-white/10",
+                  activeTab === "all" ? "h-full" : "h-max"
+                )}
+              >
+                <div className="relative aspect-square">
+                  <Image
+                    src={item.image || ""}
+                    alt="item"
+                    width={100}
+                    height={100}
+                    className="object-cover w-full h-full rounded"
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default JournalPage
