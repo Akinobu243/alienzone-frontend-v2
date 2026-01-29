2cb01e2f79b51a526e7701c084963afe49ceb8d1
Nikita Schastnyy <smynay@yandex.ru>
2026-01-29 14:52:02 +0300

    style: add styles to tabs

--- components/pages/profile/components/Tabs/Tab.tsx
+++ components/pages/profile/components/Tabs/Tab.tsx
@@ -9,8 +9,8 @@ export interface TabProps {
 
 export const Tab = ({ title, after, onClick, isActive }: TabProps) => {
   const buttonClassnames = isActive
-    ? "flex-1 bg-white/10 rounded-10 p-3 flex items-center justify-between"
-    : "flex-1 bg-white/5 hover:bg-white/10 rounded-10 p-3 flex items-center justify-between"
+    ? "flex-1 glass-effect-filled rounded-10 p-3 flex items-center justify-between"
+    : "flex-1 glass-effect duration-300 hover:bg-white/10 rounded-10 p-3 flex items-center justify-between"
 
   return (
     <button className={buttonClassnames} onClick={onClick}>
